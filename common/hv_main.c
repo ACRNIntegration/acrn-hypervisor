325d42b022ee3a101a42de6b17e9fc52eb2295be
root <root@arcn-ffshen.sh.intel.com>
2018-04-27 04:46:29 -0400

    5 pull request

--- common/hv_main.c
+++ common/hv_main.c
@@ -27,7 +27,7 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-//4 pull request
+//5` pull request
 
 #include <hypervisor.h>
 #include <hv_lib.h>
